eb9717eb8fca73e6a83662819bcdce29406e55e8
Gunho Park <devgun@kakao.com>
2020-05-18 12:32:04 +0900

    commit

---
+++ JavaScript_jQuery/11.Math/index.js
@@ -0,0 +1,36 @@
+// 원주율 값
+console.log('Math.PI=' + Math.PI);  //3.141592653589793
+
+// 주어진 값의 절대값 출력
+console.log('Math.abs(-2)=' + Math.abs(-2));    //2
+console.log('Math.abs("-2")=' + Math.abs("-2"));    //2
+
+// 주어진 값보다 같거나 큰 쪽의 정수
+console.log('Math.ceil(0.75)=' + Math.ceil(0.75));  //1
+console.log('Math.ceil(1.25)=' + Math.ceil(1.25));  //2
+console.log('Math.ceil(-1.8)=' + Math.ceil(-1.8));  //-1
+
+// 작거나 같은 정수
+console.log('Math.floor(1.25)=' + Math.floor(1.25));    //1
+console.log('Math.floor(-1.8)=' + Math.floor(-1.8));    //-2
+console.log('Math.floor(0.75)=' + Math.floor(0.75));    //0
+
+// 주어진 값에서 가까운 정수
+console.log('Math.round(0.75)=' + Math.round(0.75));    //1
+console.log('Math.round(0.45)=' + Math.round(0.45));    //0
+
+// 주어진 숫자 중 가장 큰 수 추출
+console.log('Math.max(1,2,3)=' + Math.max(1, 2, 3));    //3
+console.log('Math.min(1,2,3)=' + Math.min(1, 2, 3));    //1
+
+console.log('Math.pow(2,3)=' + Math.pow(2, 3));     //8
+
+// 0~1 사이 다른 숫자
+console.log('Math.random()=' + Math.random());
+console.log('Math.floor((Math.random()*10)+1)=' + Math.floor((Math.random() * 10) + 1));
+console.log('Math.floor((Math.random()*100)+1)=' + Math.floor((Math.random() * 100) + 1));
+
+//주어진 숫자들 중 가장 큰 수 구하기
+console.log('Math.max(1,2,3,4,5)=' + Math.max(1, 2, 3, 4, 5));  //5
+
+console.log('Math.min(1,2,3,4,5)=' + Math.min(1, 2, 3, 4, 5));  //1
