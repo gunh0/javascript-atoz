90ffa2c307bf5d3318cb378397ada7b439b85b2c
Gunho Park <devgun@kakao.com>
2020-07-15 19:44:57 +0900

    read stream

---
+++ NodeJS/1.Node_Function/5.File_System_Access/2.buffer/2_createReadStream.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+const readStream = fs.createReadStream('./sample_text.txt', {
+    highWaterMark: 16   // 16B
+});
+const data = [];
+
+readStream.on('data', (chunk) => {
+    data.push(chunk);
+    console.log('data :', chunk, chunk.length);
+});
+
+readStream.on('enc', () => {
+    console.log('end :', Buffer.concat(data).toString());
+});
+
+readStream.on('error', (err) => {
+    console.log('error :', err);
+});
