33f51271316a4b62a0e2650b26a3670976f383fe
Gunho Park <devgun@kakao.com>
2020-07-18 20:58:22 +0900

    wirte stream

---
+++ NodeJS/1.Node_Function/5.File_System_Access/2.buffer/3_createWriteStream.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+const writeStream = fs.createWriteStream('./write_sample.txt');
+writeStream.on('finish',()=>{
+    console.log('Finish, write_sample.txt');
+});
+
+writeStream.write('WRITE AGAIN.\n');
+writeStream.write('FINAL WRITE.\n');
+writeStream.end();
