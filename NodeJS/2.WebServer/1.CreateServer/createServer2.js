a7710be0436b615b35fdf7741223037be386226a
Gunho Park <devgun@kakao.com>
2020-07-28 17:05:34 +0900

    simple server code run

---
+++ NodeJS/2.WebServer/1.CreateServer/createServer2.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.write('<h1>Hello Node!</h1>');
+    res.end('<p>Hello Server!</p>');
+});
+
+server.listen(8080);
+server.on('listening', () => {
+    console.log('Currently waiting on port 8080.');
+});
+server.on('error', (error) => {
+    console.error(error);
+})
