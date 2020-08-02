072ff9dfe31591039ba5284891cb97e434729712
Gunho Park <devgun@kakao.com>
2020-08-03 06:45:38 +0900

    using cookies
        edit dd6b8ac using cookies

---
+++ NodeJS/2.WebServer/2.Cookie-Session/2_understandingCookie/server.js
@@ -0,0 +1,44 @@
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const qs = require('querystring');
+
+const parseCookies = (cookie = '') =>
+    cookie.split(';').map(v => v.split('=')).map(([k, ...vs]) => [k, vs.join('=')]).reduce((acc, [k, v]) => {
+        acc[k.trim()] = decodeURIComponent(v);
+        return acc;
+    }, {});
+
+http.createServer((req, res) => {
+        const cookies = parseCookies(req.headers.cookie);
+        if (req.url.startsWith('/login')) {
+            const {
+                query
+            } = url.parse(req.url);
+            const {
+                name
+            } = qs.parse(query);
+            const expires = new Date();
+            expires.setMinutes(expires.getMinutes() + 5);
+            res.writeHead(302, {
+                Location: '/',
+                'Set-Cookie': `name=${encodeURIComponent(name)};Expires=${expires.toGMTString()}; HttpOnly; Path=/`,
+            });
+            res.end();
+        } else if (cookies.name) {
+            res.writeHead(200, {
+                'Content-Type': 'text/html; charset=utf-8'
+            });
+            res.end(`Hello. ${cookies.name}!`);
+        } else {
+            fs.readFile('./server.html', (err, data) => {
+                if (err) {
+                    throw err;
+                }
+                res.end(data);
+            });
+        }
+    })
+    .listen(8002, () => {
+        console.log('Currently waiting on port 8002.');
+    });
